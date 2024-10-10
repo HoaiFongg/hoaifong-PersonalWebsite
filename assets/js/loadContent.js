c280a6a780c32234bc262d65da83067f4e9fb694
hoaifong <hoaif@hoaifong.com>
2024-10-10 19:34:57 +0700

    'new'

---
+++ assets/js/loadContent.js
@@ -0,0 +1,20 @@
+// Hàm để tải nội dung HTML từ một tệp
+function loadHTML(elementId, file) {
+    return fetch(file)
+        .then(response => {
+            if (!response.ok) throw new Error('Network response was not ok');
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+        })
+        .catch(error => console.error('Error loading HTML:', error));
+}
+
+// Tải header và footer
+Promise.all([
+    loadHTML('header', 'partials/header.html'),
+    loadHTML('footer', 'partials/footer.html')
+]).then(() => {
+    document.body.classList.remove('hidden'); // Hiện nội dung sau khi tải xong
+});
